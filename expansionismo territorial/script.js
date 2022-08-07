ee20cfae4950bc85612e3576ae68efefdfd4e234
oAoj1 <joaogbmarques@gmail.com>
2022-08-07 18:05:00 -0300

    resumos: 1 guerra, revolucao russa

--- expansionismo territorial/script.js
+++ expansionismo territorial/script.js
@@ -1,51 +1,63 @@
-var fle = document.querySelectorAll('.flecha')
 var fec = document.querySelectorAll('.fechar')
 
 for(var c in fec){
     //selecionar cada item de FECHAR e de FLECHA
     var fechar = fec[c]
-    var flecha = fle[c]
 
     //selecionar as LI dos CONTEUDOS
-    var expansionismo = document.querySelector('.exp')
+    var francisco = document.querySelector('#francisco')
+    var vladimir = document.querySelector('#vladimir')
     var revolucao = document.querySelector('.rev')
     var guerra = document.querySelector('.gue')
 
     //selecionar os RESULTADOS dos conteudos
-    var res_expansionismo = document.querySelector('#res_exp') 
+    var res_vladimir = document.querySelector('#res_vlad')
+    var res_francisco = document.querySelector('#res_fran')
     var res_revolucao = document.querySelector('#res_rev')
     var res_guerra = document.querySelector('#res_gue')
 
     //funcao FECHAR
+    var fec_fran = document.querySelector('.fechar-fran')
+    var fec_vlad = document.querySelector('.fechar-vlad')
     fechar.onclick = function(){
-        res_expansionismo.classList.remove('ativar')
+        res_vladimir.classList.remove('ativar')
         res_revolucao.classList.remove('ativar')
         res_guerra.classList.remove('ativar')
+        res_francisco.classList.remove('ativar')
     }
 
-    //expansionismo
-    expansionismo.onclick = function(){
-        res_expansionismo.classList.toggle('ativar')
-        res_revolucao.classList.remove('ativar')
-        res_guerra.classList.remove('ativar')
+    fec_fran.onclick = function(){
+        res_francisco.classList.remove('ativar')
+    }
+    fec_vlad.onclick = function(){
+        res_vladimir.classList.remove('ativar')
     }
 
     //revolucao
     revolucao.onclick = function(){
         res_revolucao.classList.toggle('ativar')
-        res_expansionismo.classList.remove('ativar')
         res_guerra.classList.remove('ativar')
+        res_francisco.classList.remove('ativar')
+        res_vladimir.classList.remove('ativar')
     }
 
     //guerra
     guerra.onclick= function(){
         res_guerra.classList.toggle('ativar')
-        res_expansionismo.classList.remove('ativar')
         res_revolucao.classList.remove('ativar')
+        res_francisco.classList.remove('ativar')
+        res_vladimir.classList.remove('ativar')
     }
 
-    //
+    //francisco
+    francisco.onclick = function(){
+        res_francisco.classList.toggle('ativar')
+    }
+
+    //vladimir
+    vladimir.onclick = function(){
+        res_vladimir.classList.toggle('ativar')
+    }
+
 }
-
-
 
