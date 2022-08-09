633ef8e30f3901e35035113b85843b526c6ced07
oAoj1 <joaogbmarques@gmail.com>
2022-08-09 20:42:13 -0300

    funcoes js

--- expansionismo territorial/script.js
+++ expansionismo territorial/script.js
@@ -1,63 +1,23 @@
-var fec = document.querySelectorAll('.fechar')
 
-for(var c in fec){
-    //selecionar cada item de FECHAR e de FLECHA
-    var fechar = fec[c]
+var guerra = document.querySelector('.gue')
+var res_guerra = document.querySelector('#res_gue')
+var fechar_gue = document.querySelector('.fechar')
 
-    //selecionar as LI dos CONTEUDOS
-    var francisco = document.querySelector('#francisco')
-    var vladimir = document.querySelector('#vladimir')
-    var revolucao = document.querySelector('.rev')
-    var guerra = document.querySelector('.gue')
+var francisco = document.querySelector('#francisco')
+var res_francisco = document.querySelector('#res_fran')
+var fechar_fran = document.querySelector('.fechar-fran')
 
-    //selecionar os RESULTADOS dos conteudos
-    var res_vladimir = document.querySelector('#res_vlad')
-    var res_francisco = document.querySelector('#res_fran')
-    var res_revolucao = document.querySelector('#res_rev')
-    var res_guerra = document.querySelector('#res_gue')
+/* */
 
-    //funcao FECHAR
-    var fec_fran = document.querySelector('.fechar-fran')
-    var fec_vlad = document.querySelector('.fechar-vlad')
-    fechar.onclick = function(){
-        res_vladimir.classList.remove('ativar')
-        res_revolucao.classList.remove('ativar')
-        res_guerra.classList.remove('ativar')
-        res_francisco.classList.remove('ativar')
+function pesquisa(tema,resultado1=0,resultado2=0){
+    var abrindo = tema.onclick = function(){
+        resultado1.classList.toggle('ativar')
+        resultado2.classList.remove('ativar') 
     }
-
-    fec_fran.onclick = function(){
-        res_francisco.classList.remove('ativar')
-    }
-    fec_vlad.onclick = function(){
-        res_vladimir.classList.remove('ativar')
-    }
-
-    //revolucao
-    revolucao.onclick = function(){
-        res_revolucao.classList.toggle('ativar')
-        res_guerra.classList.remove('ativar')
-        res_francisco.classList.remove('ativar')
-        res_vladimir.classList.remove('ativar')
-    }
-
-    //guerra
-    guerra.onclick= function(){
-        res_guerra.classList.toggle('ativar')
-        res_revolucao.classList.remove('ativar')
-        res_francisco.classList.remove('ativar')
-        res_vladimir.classList.remove('ativar')
-    }
-
-    //francisco
-    francisco.onclick = function(){
-        res_francisco.classList.toggle('ativar')
-    }
-
-    //vladimir
-    vladimir.onclick = function(){
-        res_vladimir.classList.toggle('ativar')
-    }
-
+    return abrindo
 }
 
+var gue = pesquisa(guerra,res_guerra,res_francisco)
+var fran = pesquisa(francisco,res_francisco)
+var fg = pesquisa(fechar_gue,res_guerra)
+var ff = pesquisa(fechar_fran,res_francisco)
