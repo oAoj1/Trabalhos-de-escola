ee6eace39984ceb13d9ceb91ecb467b17583a9ae
oAoj1 <joaogbmarques@gmail.com>
2022-08-19 20:43:05 -0300

    conteudo 1.0

--- biologia/js/script.js
+++ biologia/js/script.js
@@ -22,3 +22,24 @@ botao.addEventListener('click',voltar)
 function voltar(){
     window.scroll(0,0)
 }
+
+/* */
+
+//puxando do HTML para JS (DOM)
+var ordens_lista = document.querySelector('.ordens')
+var oquesao = document.querySelector('#oquesao')
+var mais_oquesao = document.querySelector('.mais_oquesao')
+var res_ordens = document.querySelector('#resultado_ordens')
+var res_oquesao = document.querySelector('#resultado_oquesao')
+
+//colocando as FUNCOES
+var resultado_ordens = ativar(ordens_lista,res_ordens)
+var resultado_oquesao = ativar(oquesao,res_oquesao,mais_oquesao)
+
+//criando as FUNCOES
+function ativar(objeto,resultado,girar=0){
+    objeto.onclick = function(){
+        resultado.classList.toggle('ativar')
+        girar.classList.toggle('girar')
+    }
+}
