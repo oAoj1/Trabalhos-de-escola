881a454cd5dc7f8178824765744772d9678a1bf7
oAoj1 <joaogbmarques@gmail.com>
2022-08-27 20:21:23 -0300

    Update animais.js

--- biologia/js/animais.js
+++ biologia/js/animais.js
@@ -21,43 +21,67 @@ function enviar(){
     }
 
     if(animais.length == 0){
+
         resultado_animais.innerHTML = '<p class="insert">Insira um valor</p>'
         img.src = ''
         img.classList.remove('img-axolote','img-sapo','img-ra','img-salamandra','img-cobra-cega')
+
     }else if(animais == lista_animais.axolote){
+
         res_animal('Urodela (Caudata)','Ambystomidae','Ambyostoma','Ambyostoma mexicanum','Ambientes escuros e de água doce','Girinos e pequenos invertebrados (insetos, crustáceos e minhocas)')
+
         titulo_animal.textContent = 'Axolote'
         img.src = 'imagens/animais/axolote.jpg'
         img.classList.add('img-axolote')
+
     }else if(animais == lista_animais.sapo[0] || animais == lista_animais.sapo[1]){
+
         res_animal('Anura','Bufonidae','Rhinella','R. marinaa','Locais úmidos, como brejos, riachos, lagoas e igarapés','Aranhas, besouros, pequenos gafanhotos, moscas, formigas e cupins. Espécies maiores podem comer pequenos pássaros e até outros sapos')
+
         titulo_animal.textContent = 'Sapo-boi'
         img.src = 'imagens/animais/sapo.webp'
         img.classList.add('img-sapo')
+
     }else if(animais == lista_animais.ra[0] || animais == lista_animais.ra[1]){
+
         res_animal('Anura','Ranidae','Ranoidea','Agalychnis callidryas ','Vivem perto de corpos d`água, lagos, lagoas, cachoeiras e pântanos','Pequenos artrópodes, insetos, vermes e outros animais')
+
         titulo_animal.textContent = 'Rã'
         img.src = 'imagens/animais/ra.jpg'
         img.classList.add('img-ra')
+
     }else if(animais == lista_animais.salamandra){
+
         res_animal('Urodela','Salamandridae','Salamandra','Salamandra salamandra','Algumas espécies vivem em cavernas; temperatura e umidade constantes favorecem o habitat das espécies','Pequenos animais, como insetos, larvas de outros animais, crustáceos, pequenos peixes, entre outros.')
+
         titulo_animal.textContent = 'Salamandra'
         img.src = 'imagens/animais/salamandra.jpg'
         img.classList.add('img-salamandra')
+
     }else if(animais == lista_animais.cobra_cega[0] || animais == lista_animais.cobra_cega[1]){
+
         res_animal('Gymnophiona','Typhlopidae','Lissanfíbio','Sinphonops annulatus','vivem enterrados (hábito fossorial) ou na superfície de solos úmidos','Vivem enterrados (hábito fossorial) ou na superfície de solos úmidos','Pequenos invertebrados')
+
         titulo_animal.textContent = 'Cobra-cega'
         img.src = 'imagens/animais/cobra-cega.webp'
         img.classList.add('img-cobra-cega')
+
     }else{
-        resultado_animais.innerHTML = '<p class="error">Animal não listado, ou valor inexistente ou incorreto</p>'
+
+        resultado_animais.innerHTML = 
+        `<p class="error">
+            Animal não listado, ou valor inexistente ou incorreto
+        </p>`
+        
         resultado_animais.innerHTML += 
         `<ul class="dica">
             <li>Seu animal não é um anfibio</li>
         </ul>`
+
         img.src = ''
         titulo_animal.textContent = ''
         img.classList.remove('img-axolote','img-sapo','img-ra','img-salamandra','img-cobra-cega')
+
     }
 
     a.value = ''
