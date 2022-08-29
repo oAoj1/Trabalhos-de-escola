19ec70740a60fc67a921a9f42ecfee677561fc9c
oAoj1 <joaogbmarques@gmail.com>
2022-08-29 05:07:38 -0300

    final version 7.0

--- biologia/js/animais.js
+++ biologia/js/animais.js
@@ -87,6 +87,71 @@ function enviar(){
         dados_animais('Cobra-cega','cobra-cega.webp','img-cobra-cega')
 
     }
+    if(animais == lista_animais.axolote){
+
+        res_animal(
+            'Urodela (Caudata)',
+            'Ambystomidae',
+            'Ambyostoma',
+            'Ambyostoma mexicanum',
+            'Ambientes escuros e de água doce',
+            'Girinos e pequenos invertebrados (insetos, crustáceos e minhocas)'
+        )
+
+        dados_animais('Axolote','axolote.jpg','img-axolote')
+
+    }else if(animais == lista_animais.sapo[0] || animais == lista_animais.sapo[1]){
+
+        res_animal(
+            'Anura',
+            'Bufonidae',
+            'Rhinella',
+            'R. marinaa',
+            'Locais úmidos, como brejos, riachos, lagoas e igarapés',
+            'Aranhas, besouros, pequenos gafanhotos, moscas, formigas e cupins. Espécies maiores podem comer pequenos pássaros e até outros sapos'
+        )
+
+        dados_animais('Sapo-boi','sapo.webp','img-sapo')
+
+    }else if(animais == lista_animais.ra[0] || animais == lista_animais.ra[1]){
+
+        res_animal(
+            'Anura',
+            'Ranidae',
+            'Ranoidea',
+            'Agalychnis callidryas ',
+            'Vivem perto de corpos d`água, lagos, lagoas, cachoeiras e pântanos',
+            'Pequenos artrópodes, insetos, vermes e outros animais'
+        )
+
+        dados_animais('Rã','ra.jpg','img-ra')
+
+    }else if(animais == lista_animais.salamandra){
+
+        res_animal(
+            'Urodela',
+            'Salamandridae',
+            'Salamandra','Salamandra salamandra',
+            'Algumas espécies vivem em cavernas; temperatura e umidade constantes favorecem o habitat das espécies',
+            'Pequenos animais, como insetos, larvas de outros animais, crustáceos, pequenos peixes, entre outros.'
+        )
+
+        dados_animais('Salamandra','salamandra.jpg','img-salamandra')
+
+    }else if(animais == lista_animais.cobra_cega[0] || animais == lista_animais.cobra_cega[1]){
+
+        res_animal(
+            'Gymnophiona',
+            'Typhlopidae',
+            'Lissanfíbio',
+            'Sinphonops annulatus',
+            'vivem enterrados (hábito fossorial) ou na superfície de solos úmidos','Vivem enterrados (hábito fossorial) ou na superfície de solos úmidos',
+            'Pequenos invertebrados'
+        )
+
+        dados_animais('Cobra-cega','cobra-cega.webp','img-cobra-cega')
+
+    }
 
     focar()
     
@@ -97,28 +162,8 @@ function enviar(){
 
 //FUNCOES
 
-
-function focar(){
-    a.value = ''
-    a.focus()
-}
-
-function remover(){
-    event.preventDefault()
-
-    limpar()
-    focar()
-}
-
-function limpar(){  
-    resultado_animais.innerHTML = ''
-    img.src = ''
-    titulo_animal.innerHTML = ''
-    img.classList.remove('img-axolote','img-sapo','img-ra','img-salamandra','img-cobra-cega')
-}
-
 function res_animal(ordem,familia,genero,especie,habitat,alimentacao){
-    return resultado_animais.innerHTML += `
+    return resultado_animais.innerHTML = `
     <ul class="resultado-lista-animais">
         <li><strong>Reino:</strong> Animalia</li>
         <li><strong>Filo:</strong> Chordata</li>
@@ -138,14 +183,21 @@ function dados_animais(titulo,imagem,classe_foto){
     img.classList.add(`${classe_foto}`)
 }
 
-function funcao_listaAnimais(){
-    var lista_animais = {
-        axolote:'axolote',
-        sapo:['sapo-boi','sapoboi'],
-        ra:['ra','rã'],
-        salamandra:'salamandra',
-        cobra_cega:['cobra-cega','cobracega'],
-    }
+function focar(){
+    a.value = ''
+    a.focus()
+}
 
-    return lista_animais
+function remover(){
+    event.preventDefault()
+
+    limpar()
+    focar()
+}
+
+function limpar(){  
+    resultado_animais.innerHTML = ''
+    img.src = ''
+    titulo_animal.innerHTML = ''
+    img.classList.remove('img-axolote','img-sapo','img-ra','img-salamandra','img-cobra-cega')
 }
---
+++ biologia/js/resultado_animais.js
@@ -0,0 +1,60 @@
+/*
+<ul class="resultado-lista-animais">
+    <li><strong>Reino:</strong> Animalia</li>
+    <li><strong>Filo:</strong> Chordata</li>
+    <li><strong>Classe:</strong> Amphibia</li>
+    <li><strong>Ordem:</strong> ${ordem}</li>
+    <li><strong>Familia:</strong> ${familia}</li>
+    <li><strong>Gênero:</strong> ${genero}</li>
+    <li><strong>Espécie:</strong> ${especie}</li>
+    <li><strong>Habitat:</strong> ${habitat}</li>
+    <li><strong>Alimentação:</strong> ${alimentacao}</li>  
+</ul>
+
+
+function res_animal2(ordem,familia,genero,especie,habitat,alimentacao){
+    return resultado_animais.textContent = `
+    <ul class="resultado-lista-animais">
+        <li><strong>Reino:</strong> Animalia</li>
+        <li><strong>Filo:</strong> Chordata</li>
+        <li><strong>Classe:</strong> Amphibia</li>
+        <li><strong>Ordem:</strong> ${ordem}</li>
+        <li><strong>Familia:</strong> ${familia}</li>
+        <li><strong>Gênero:</strong> ${genero}</li>
+        <li><strong>Espécie:</strong> ${especie}</li>
+        <li><strong>Habitat:</strong> ${habitat}</li>
+        <li><strong>Alimentação:</strong> ${alimentacao}</li>  
+    </ul>`
+}
+
+var axolote_resultado1 = `
+<ul class="resultado-lista-animais">
+        <li><strong>Reino:</strong> Animalia</li>
+        <li><strong>Filo:</strong> Chordata</li>
+        <li><strong>Classe:</strong> Amphibia</li>
+        <li><strong>Ordem:</strong> Urodela (Caudata)</li>
+        <li><strong>Familia:</strong> Ambystomidae</li>
+        <li><strong>Gênero:</strong> Ambyostoma</li>
+        <li><strong>Espécie:</strong> Ambyostoma mexicanum</li>
+        <li><strong>Habitat:</strong> Ambientes escuros e de água doce</li>
+        <li><strong>Alimentação:</strong> Girinos e pequenos invertebrados (insetos, crustáceos e minhocas)
+        </li>  
+</ul>`
+
+
+
+var axolote_resultado2 = {
+
+    Reino:'Animalia',
+    Filo:'Chordata',
+    Classe:'Amphibia',
+    Ordem:'Urodela',
+    Familia:'Ambystomidae',
+    Espécie:'Ambyostoma mexicanum',
+    Habitat:'Ambientes escuros e de água doce',
+    Alimentação:'Girinos e pequenos invertebrados (insetos, crustáceos e minhocas)'
+
+} 
+
+console.log(axolote_resultado2);
+*/
