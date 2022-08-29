43914e1db59defc7f848a9fc98a9e24099e199d2
oAoj1 <joaogbmarques@gmail.com>
2022-08-29 18:43:39 -0300

    final version 10.0

--- biologia/js/abrir_fechar.js
+++ biologia/js/abrir_fechar.js
@@ -1,6 +1,6 @@
 //FUNCOES
 
-function ativar(objeto,resultado,girar=0){
+function ativar(objeto=0,resultado=0,girar=0){
     objeto.onclick = function(){
         resultado.classList.toggle('ativar')
         girar.classList.toggle('girar')
@@ -9,30 +9,27 @@ function ativar(objeto,resultado,girar=0){
 
 /*resultados */
 var res_oquesao = document.querySelector('#resultado_oquesao')
-var res_ordens = document.querySelector('#resultado_ordens')
 var res_gerais = document.querySelector('#resultado_gerais')
 var res_respirar = document.querySelector('#resultado_respiracao')
 var res_digestao = document.querySelector('#resultado_digestao')
 var res_reproducao = document.querySelector('#resultado_reproducao')
 
-/* plus*/
+/* girar*/
 var mais_gerais = document.querySelector('.mais_gerais')
 var mais_oquesao = document.querySelector('.mais_oquesao')
 var mais_respirar = document.querySelector('.mais_respiracao')
 var mais_digestao = document.querySelector('.mais_digestao')
 var mais_reproducao = document.querySelector('.mais_reproducao')
 
-/*itens */
-var ordens_lista = document.querySelector('.ordens')
+/*objeto */
 var oquesao = document.querySelector('#oquesao')
 var gerais = document.querySelector('#gerais')
 var respiracao = document.querySelector('#respiracao')
 var digestao =  document.querySelector('#digestao')
 var reproducao = document.querySelector('#reproducao')
 
-//colocando as FUNCOES
+//aplicando as FUNCOES
 var resultado_oquesao = ativar(oquesao,res_oquesao,mais_oquesao)
-var resultado_ordens = ativar(ordens_lista,res_ordens)
 var resultado_gerais = ativar(gerais,res_gerais,mais_gerais)
 var resultado_respiracao = ativar(respiracao,res_respirar,mais_respirar)
 var resultado_digestao = ativar(digestao,res_digestao,mais_digestao)
